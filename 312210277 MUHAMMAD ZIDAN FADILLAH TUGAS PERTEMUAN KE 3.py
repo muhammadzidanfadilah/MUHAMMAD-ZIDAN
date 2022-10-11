0a4bb193782582d9fd5848e95e240811be9bc2fa
muhammadzidanfadillah <115553474+muhammadzidanfadilah@users.noreply.github.com>
2022-10-11 20:42:40 +0700

    Add files via upload

---
+++ 312210277 MUHAMMAD ZIDAN FADILLAH TUGAS PERTEMUAN KE 3.py
@@ -0,0 +1,25 @@
+#Bahasa pemrograman python
+
+#Membuat a string
+#Dengan single Quotes
+pesan1 = 'cinta seorang ibu adalah sesuatu yang sangat berarti'
+print("Tidak ada yang bisa menjelaskannya:")
+print(pesan1)
+
+#membuat String
+#Dengan double Quotes
+pesan2 = "cinta seorang ibu itu terbuat dari pengabdian yang mendalam"
+print("\nTidak akan pernah ada habisnya sampai kapanpun:")
+print(pesan2)
+
+#membuat string
+#Dengan tiga Quotes
+pesan3 = '''karena tidak ada yang bisa'''
+print("\nMenghancurkan atau mengambil cinta itu:")
+print(pesan3)
+
+#Creating String With Triple
+#Quotes allows multiple lines
+pesan4 = '''dan cinta ibu bersinar dengan segala keindahannya'''
+print("\nDari permata yang paling langka dan paling cemerlang:")
+print(pesan4)
